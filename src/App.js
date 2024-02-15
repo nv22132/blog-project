5e1cea5e7eeaceee80a46047e35671e8d32d677d
nv22132 <150009692+nv22132@users.noreply.github.com>
2024-02-15 06:56:32 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './component/Card';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
