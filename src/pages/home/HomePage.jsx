50ee6a8b72e83c8c22bd44182797e4e4447a38f6
nv22132 <150009692+nv22132@users.noreply.github.com>
2024-03-09 18:03:16 +0000

    articals card

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-
 import MainLayout from "../../component/MainLayout.jsx";
 import Hero from "./container/hero";
+import Articles from "./container/Articles";
+
 
 const HomePage = () => {
   return (
     <MainLayout>
       <Hero />
+      <Articles />
     </MainLayout>
   );
 };
