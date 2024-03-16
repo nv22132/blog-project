829c4540cb0de2473975f6777d12d46ce93db6d9
nv22132 <150009692+nv22132@users.noreply.github.com>
2024-03-16 08:42:27 +0000

    CTA section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import MainLayout from "../../component/MainLayout.jsx";
 import Hero from "./container/hero";
 import Articles from "./container/Articles";
+import CTA from "./container/CTA";
+
 
 
 const HomePage = () => {
@@ -9,6 +11,7 @@ const HomePage = () => {
     <MainLayout>
       <Hero />
       <Articles />
+      <CTA />
     </MainLayout>
   );
 };
