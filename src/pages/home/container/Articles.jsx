2c2b5b239c4808618e5b4cad4299c7cc1199afab
nv22132 <150009692+nv22132@users.noreply.github.com>
2024-06-01 04:32:42 +0000

    redux

--- src/pages/home/container/Articles.jsx
+++ src/pages/home/container/Articles.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import ArticleCard from "../../../component/ArticleCard";
 import { FaArrowRight } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+
+import ArticleCard from "../../../component/ArticleCard";
+import { changeCount } from "../../../store/actions/countActions";
 
 const Articles = () => {
+  const dispatch = useDispatch()
+  const count = useSelector((state) => state.count);
+
+  const countChangeHandler = (type) => {
+    dispatch(changeCount(type))
+  }
   return (
     <section className="flex flex-col container mx-auto px-5 py-10">
     <div className=" flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
@@ -13,6 +22,11 @@ const Articles = () => {
       <span>More articles</span>
       <FaArrowRight className="w-3 h-3" />
     </button>
+    <div className="mt-2 flex items-center gap-x-5">
+        <button onClick={() => countChangeHandler('DECREASE')}>decrease</button>
+        {count.number}
+        <button onClick={() => countChangeHandler('INCREASE')}>increase</button>
+      </div>
     </section>
   );
 };
